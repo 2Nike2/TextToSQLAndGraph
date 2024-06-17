a86cadaae2db1de3f929d83a14baf2e924d77784
2Nike2 <29421346+2Nike2@users.noreply.github.com>
2024-06-17 23:07:59 +0900

    円グラフ描画の為の調査、及びプロンプトの改善

--- models/datasets.py
+++ models/datasets.py
@@ -17,15 +17,19 @@ spark = SparkSession.builder.appName('dataset')\
     .config("spark.sql.warehouse.dir", warehouse_path) \
     .enableHiveSupport().getOrCreate()
 
+# 円グラフでは、ラベルの列のデータ型が文字列でないといけない上に、
+# PySparkがPandasのデータフレームに変換する際に勝手にデータ型を変えてしまう恐れがあるので、
+# ラベルの列名を指定して、この列名を使ってデータ型を文字列で明示的に変換する処理を実装して対応。
 create_sql_prompt = """\
 与えられた指示に適切なSQLクエリをテーブル定義を利用しつつ回答して下さい。
 
 制約:
 ・出力をそのままSQL文として実行するので、余計な文言を入れずにSQL文のみを出力して下さい。
 ・インプリシットカラムアンチパターンは止めて出力の列名は必ず明記して下さい。
+・Labelや凡例の列は必ず列名を「Label」或いは「Label」を含む文字列の列名にして下さい。
 
 出力例:
-SELECT COLUMN1, COLUMN2 FROM TABLEX WHERE COLUMN3 = "XXX";
+SELECT COLUMN1 AS Label, COLUMN2 FROM TABLEX WHERE COLUMN3 = "XXX";
 
 指示: {instruction}
 
@@ -58,8 +62,16 @@ def get_sql_data(instruction):
     print('create_sql:', create_sql)
 
     result = spark.sql(create_sql)
+    # resultのデータの中身を表示
+    print('result:', result)
+    print('result_show:')
+    result.show()
 
-    # この辺りで列順がおかしくなっていないか、要調査
-    df = result.toPandas().to_dict(orient="records")
-
+    df = result.toPandas()
+    print('df1:', df)
+    for col in df.columns:
+        if 'Label' in col:
+            df[col] = df[col].astype(str)
+    df = df.to_dict(orient="records")
+    print('df2:', df)
     return df
